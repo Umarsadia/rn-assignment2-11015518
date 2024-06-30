e71bcd44c544f69dda1a783a2fc68017946bb0c3
Umarsadia <hsumar@st.ug.edu.gh>
2024-06-30 16:15:35 +0000

    changed background colour to blue

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>my name is halimatu sadia umar</Text>
+      <Text>My name is Halimatu Sadia Umar</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
